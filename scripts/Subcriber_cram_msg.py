def0523fcb3935bd6a110ebd586c337992f4d353
MehreenNaeem <106755148+MehreenNaeem@users.noreply.github.com>
2023-05-05 15:13:19 +0200

    Add files via upload

--- scripts/Subcriber_cram_msg.py
+++ scripts/Subcriber_cram_msg.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python
+
+from gpsr_robocup.DataEntering.Data_entering import CmdIntre
 import rospy
 from std_msgs.msg import String
+from gpsr_robocup.Audioreader import MyAudioreader
+from gpsr_robocup import _nlpCommands
 
-def CRAMoutput(word):
-       print(word)
-       return word
+def callback2(data2):
+    global checkvar
+    checkvar = data2.commands
 
+global cram_speaker
+cram_speaker = []
 def callback(data):
        cram_cmd = data.data
-       CRAMoutput(cram_cmd)
+       PlanList = checkvar
+       if cram_cmd == 'done':
+           print('----')
+           print(cram_cmd)
+           print('----')
+       if cram_cmd in PlanList:
+           cram_speaker.append(cram_cmd)
+           print(cram_speaker)
+       if cram_speaker == PlanList:
+           MyAudioreader.speak("I have done the task")
+           MyAudioreader.speak("Going back to initial position")
+           pubwords = rospy.Publisher('NLPchatter',_nlpCommands.nlpCommands, queue_size=10)
+           (rospy.Rate(5)).sleep()
+           for i in range(1) : pubwords.publish(['Done','Task'])
+           cram_speaker.clear()
+
 rospy.init_node('CRAM_listener', anonymous=True)
-cram_listner = rospy.Subscriber("gpsrpub", String, callback)
+cram_listner = rospy.Subscriber("CRAMpub", String, callback)
+cram_listner = rospy.Subscriber('PlanList', _nlpCommands.nlpCommands, callback2)
 rospy.spin()
